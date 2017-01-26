27a41d65c8772ddb699e92edc6f9f34061abd4ae
Colander <281137@bsd48.org>
2017-01-26 10:27:07 -0800

    starting my evil plan

--- DWprisoners_dilemma.py
+++ DWprisoners_dilemma.py
@@ -138,6 +138,88 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
             return 'EJS'
         else:
             return 'b'
+          elif player == 8:
+        if getting_team_name:
+            #if there was a previous round just like 
+            return 'loyal vengeful with permanent second impression'
+        else:
+            # use history, opponent_history, score, opponent_score
+            # to compute your strategy      
+            if len(opponent_history)==0: #It's the first round: collude
+                return 'c'
+            else:
+                # if there was a previous round just like the last one,
+                # do whatever they did in the round that followed it
+                recent_round_opponent = opponent_history[-1]
+                recent_round_me = history[-1]
+                            
+                #go through rounds before that one
+                for round in range(len(history)-1):
+                    prior_round_opponent = opponent_history[round]
+                    prior_round_me = history[round]
+                    #if one matches
+                    if (prior_round_me == recent_round_me) and \
+                            (prior_round_opponent == recent_round_opponent):
+                        return opponent_history[round]
+                # no match found
+                if history[-1]=='c' and opponent_history[-1]=='b':
+                    return 'b' # betray if they were severely punished last time
+                else:
+                    return 'c' #otherwise collude
+
+ elif player == 10:
+        if getting_team_name:
+            return 'temporary clever team name that is totally unique and creative'
+        else:
+            # use history, opponent_history, score, opponent_score
+            # to compute your strategy
+            if len(opponent_history)==0: #It's the first round: collude
+                return 'c'
+            elif history[-1]=='c' and opponent_history[-1]=='b':
+                return 'b' # betray is they were severely punished last time
+            else:
+                return 'c' #otherwise collude
+
+ elif player == 10:
+        if getting_team_name:
+            return 'Possible AI without short-term memory loss'
+        else:
+            # use history, opponent_history, score, opponent_score
+            # to compute your strategy
+            if len(opponent_history)==0: #It's the first round: collude
+                return 'c'
+            elif opponent_history[-1]=='b':
+                return 'b' # betray is they were severely punished last time
+            else:
+                return 'c' #otherwise collude
+                
+            moves_table[player1][player2] = moves1
+            moves_table[player2][player1] = moves2
+            filename = os.path.join(directory, 'tournament.txt')
+
+
+    team_names = range(num_players)
+   
+    use_datafile=True
+    if use_datafile:
+        # use the same directory as the python script
+        import os.path              
+        directory = os.path.dirname(os.path.abspath(__file__))  
+        
+        #name the file tournament.txt
+        filename = os.path.join(directory, 'tournament.txt')
+        #create the file for the round-by-round results
+        results = open(filename,'w')
+        for player1 in range(num_players):
+            for player2 in range(player1):
+              
+        #append the file showing algorithms
+        results.write('\n\n' + '-'*79 + '\n' + \
+                    'Here is the code that produced this data:\n\n')
+        this_code_file = open(__file__, 'r')
+        for line in this_code_file:
+            results.write(line)
+                
 
 
 
